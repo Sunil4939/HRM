dc1cb760485217f602f39a68d255be777d9057ce
Sunil Prajapati <prajapatisunil9777@gmail.com>
2022-12-26 16:59:25 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Root from "./Root";
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  },[])
+ 
   return(
     <Root />
   )
